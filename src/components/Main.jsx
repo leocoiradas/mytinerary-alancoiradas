991b58592fdd783e7ce0d64b9e75de8465aec32c
Alan Coiradas <coiradasalan@gmail.com>
2023-08-09 20:54:03 -0300

    Cambios estéticos
    Se realizaron cambios estéticos en los distintos componentes de la página

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,14 @@ import cities from '../assets/data/Cities'
 function Main(){
     const welcome = [{
         title: 'Find the perfect destination',
-        description: 'Our App will help you find the perfect path for your next trip. With an easy-to-use interface, and a host of itinerary options, planning your next trip has never been easier',
+        description: 'Our App will help you find the perfect path for your next trip. With an easy-to-use interface, and a host of itinerary options, planning your next trip has never been easier.',
+        slogan: 'Find your perfect trip, designed by insiders who know and love their cities.' ,
         image: 'src/assets/images/default-image.jpg'  
     }]
     return(
-        <main className="flex flex-col justify-center items-center min-h-[80vh] p-5 gap-6">
+        <main className="flex flex-col justify-center items-center min-h-[80vh] p-5 gap-12">
             <section className="h-[90vh] flex justify-center items-center">
-            {welcome.map((component, i)=> (<Info key={i} title={component.title} desc={component.description} img={component.image} />))}
+            {welcome.map((component, i)=> (<Info key={i} title={component.title} desc={component.description} slogan={component.slogan} img={component.image} />))}
             </section>
             <section>
                 <Carousel arr={cities} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar({ isOpen }) {
     return (
         <nav className={`${isOpen ? 'block text-center' : 'hidden'} md:flex md:items-center md:w-auto`}>
             <ul className={`flex flex-col md:flex-row md:gap-4  ${isOpen ? 'block gap-2 md:flex' : 'hidden'} md:flex w-full`}>
-                {links.map((link) => (<li key={link.title}><Anchor className={`p-1.5 rounded-lg text-lg md:p-3 md:text-base hover:bg-blue-500  ${link.isUser ? ' bg-blue-700 text-zinc-100  hover:bg-violet-500' : ''}`} to={link.to}>{link.title}</Anchor></li>))}
+                {links.map((link) => (<li key={link.title}><Anchor className={`p-1.5 rounded-lg text-lg md:p-3 md:text-base hover:bg-blue-500 hover:text-white  ${link.isUser ? ' bg-blue-700 text-zinc-100  hover:bg-violet-500' : ''}`} to={link.to}>{link.title}</Anchor></li>))}
             </ul>
         </nav>
     );
