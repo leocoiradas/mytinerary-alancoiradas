3c3d61bb2e8d5c4d63710c87b69deb53932713d3
Alan Coiradas <coiradasalan@gmail.com>
2023-08-10 02:16:28 -0300

    Footer
    Se agregaron elementos al footer a modo de anti copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 function Footer (){
+    
     return(
-        <footer className="bg-blue-500 p-3 min-h-[10vh] mt-2 w-full">
-            <p>developed by alan</p>
+        <footer className="bg-blue-500 p-3 min-h-[10vh] mt-2 w-full flex">
+            <div className="flex flex-col items-start text-white">
+                <p>Developed by Alan</p>
+                <p>All rights reserved.</p>
+            </div>
         </footer>
     );
 };
