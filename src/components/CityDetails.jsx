e58f5b0138fd636545945ea9fd15b3bac828c3e0
Alan Coiradas <coiradasalan@gmail.com>
2023-08-22 03:22:29 -0300

    Cambios e implementaciones en CityDetails
    Se hicieron modificaciones estéticas en la Card, se implementaron el texto que dice "Under Construction" así como un botón funcional que nos devuelve a Cities.

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -19,16 +19,23 @@ function CityDetails() {
     console.log(cityDetails)
     return (
         <section className="min-h-[80vh] p-10 flex justify-center items-center">
-            <article className="w-[85vw] flex justify-center items-center">
+            <article className="w-[85vw] flex flex-col justify-center items-center gap-6">
                 {cityDetails ? (
-                    <div className="rounded-md flex flex-col justify-center items-center gap-3">
+                    <div className="rounded-md flex flex-col justify-center items-center gap-3 m-4 border-2 border-neutral-950 p-4">
                         <h2 className="text-4xl">{cityDetails.cityName}</h2>
-                        <img className="w-96 h-auto object-cover rounded-md"src={cityDetails.img} alt={`City of ${cityDetails.cityName}`} />
+                        <img className="w-96 h-auto object-cover rounded-md" src={cityDetails.img} alt={`City of ${cityDetails.cityName}`} />
                         <p>Country: {cityDetails.country}</p>
                     </div>
                 ) : (
                     <p>Loading...</p>
                 )}
+                <div className="p-3 flex flex-col gap-3">
+                    <h3 className="text-xl italic">Under construction</h3>
+                    <Link to="/cities" className="btn bg-cyan-400 hover:bg-blue-800 hover:text-white text-center p-3 rounded-md text-md">
+                        Back to Cities
+                    </Link>
+                </div>
+
             </article>
         </section>
 
