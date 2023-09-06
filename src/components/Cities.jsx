b2029eb60c2c44d6693aa5af125b5cc9ae00a3a3
Alan Coiradas <coiradasalan@gmail.com>
2023-09-06 03:22:42 -0300

    Redux
    Se implementó el uso de Redux en el componente Cities para pasar de estados locales a globales.

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,32 +1,39 @@
 
-import { useState, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Card from './Card';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { filter_cities, get_cities } from '../store/actions/citiesAction';
 
 function Cities() {
-    const [cities, setCities] = useState()
+    const cities = useSelector((store) => store.citiesReducer.cities)
+    const dispatch = useDispatch()
+    let inputSearch = useRef()
+    //const [cities, setCities] = useState()
     useEffect(() => {
-        
-        axios.get('http://localhost:7000/api/cities')
+        dispatch(get_cities())
+       /* axios.get('http://localhost:7000/api/cities')
             .then(response => setCities(response.data.cities))
             
-            .catch((err) => console.log(err))
+            .catch((err) => console.log(err))*/
     }, [])
-    const handleInputChange = async (city) => {
-        console.log(cities)
+    const handleInputChange = async () => {
+        dispatch(filter_cities({
+            name: inputSearch.current.value
+        }))
+        /*console.log(cities)
         try {
             const response = await axios.get(`http://localhost:7000/api/cities?cityName=${city.target.value}`)
             setCities(response.data.cities)
         } catch (error) {
             console.log(error)
-        }
+        }*/
     }
     return (
         <main className='flex flex-col justify-center items-center min-h-[80vh] w-full pt-10 gap-3'>
            <h2 className='text-3xl font-medium font-sans'>Cities</h2>
             <section className='flex flex-col items-center justify-center p-3 gap-10 w-[75vw]'>
-            <input onChange={handleInputChange} className='border-2 rounded-md py-1 px-2 w-3/4 border-slate-950' type='text' placeholder='Write the city name here.' />
+            <input onChange={handleInputChange} ref={inputSearch} className='border-2 rounded-md py-1 px-2 w-3/4 border-slate-950' type='text' placeholder='Write the city name here.' />
                 <article className='flex gap-5 flex-wrap justify-center'>
                     {cities?.map( (city) => {
                         return(
