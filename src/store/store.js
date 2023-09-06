36bbee3bce60f04a806ebc9d833dca066511cebc
Alan Coiradas <coiradasalan@gmail.com>
2023-09-06 03:23:57 -0300

    Actualización de reducers
    Se actualizaron los reducers en el store agregando el nuevo reducer correspondiente a cityDetails.

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import citiesReducer from "./reducers/citiesReducer";
+import detailsReducer from "./reducers/detailsReducer";
+
 
 const store = configureStore({
     reducer: {
-
+        citiesReducer: citiesReducer,
+        detailsReducer: detailsReducer
     }
 });
 export default store 
