afa103864fb7cfe0126467c131cb21be9e18fc65
Alan Coiradas <coiradasalan@gmail.com>
2023-09-14 22:47:06 -0300

    signin y signup
    Se implementaron los componentes de signin y signup junto con sus actions y reducers de redux así como el almacenamiento de este último en el store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import citiesReducer from "./reducers/citiesReducer";
 import detailsReducer from "./reducers/detailsReducer";
+import userReducer from "./reducers/userReducer";
 
 
 const store = configureStore({
     reducer: {
         citiesReducer: citiesReducer,
-        detailsReducer: detailsReducer
+        detailsReducer: detailsReducer,
+        userReducer: userReducer
     }
 });
 export default store 
