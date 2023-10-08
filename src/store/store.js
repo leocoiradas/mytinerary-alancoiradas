843f5ec6a7bcf2d3f022cbe27946ba97056e99e9
Alan Coiradas <coiradasalan@gmail.com>
2023-10-08 11:26:10 -0300

    Creación de Actions y reducers: postear nuevo itinerario
    Se crearon la accción y reducers encargadas de enviar la petición post a itineraries para crear un nuevo itinerario. Asimismo se incorporó al store de Redux.

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import citiesReducer from "./reducers/citiesReducer";
 import detailsReducer from "./reducers/detailsReducer";
 import userReducer from "./reducers/userReducer";
+import itineraryReducer from "./reducers/itineraryReducer";
 
 
 const store = configureStore({
     reducer: {
         citiesReducer: citiesReducer,
         detailsReducer: detailsReducer,
-        userReducer: userReducer
+        userReducer: userReducer,
+        itineraryReducer: itineraryReducer
     }
 });
 export default store 
